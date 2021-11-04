7b979428c451dab4bb31b3df0aa742f7fa1ec576
nateshim <nateshim@berkeley.edu>
2021-11-04 10:38:58 -0700

    styling home

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,6 +1,16 @@
 import {Box, Container, Typography} from '@mui/material';
+import {makeStyles} from '@mui/styles';
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    color: theme.palette.text.primary,
+    fontWeight: 'bold',
+    fontSize: '100px',
+  }
+}));
 
 const Home = () => {
+  const classes = useStyles();
   return (
     <Container>
         <Box 
@@ -9,7 +19,15 @@ const Home = () => {
           alignItems="center" 
           justifyContent="center"
         >
-          <Typography color="textPrimary">Organize your conversations wherever and whenener.</Typography>
+          <Typography 
+            color='textPrimary' 
+            style={{
+              fontWeight: 'bold',
+              fontSize: '50px',
+            }}
+          >
+            Organize your conversations wherever, whenever.
+          </Typography>
         </Box>
         <Box 
           display="flex" 
