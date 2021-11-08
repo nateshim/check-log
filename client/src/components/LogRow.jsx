09a29376e8e9e22d91450644a4a13c60570d4fd2
nateshim <nateshim@berkeley.edu>
2021-11-08 07:30:36 -0800

    adding scroll

--- client/src/components/LogRow.jsx
+++ client/src/components/LogRow.jsx
@@ -12,16 +12,16 @@ const LogRow = (props) => {
   return (
     <>
       <Grid container item spacing={1} style={(props.id === 0 ? {background: '#331E36'} : {background: 'none'})}>
-        <Grid item xs={2}> 
+        <Grid item xs={2} style={{maxHeight: '15vh', overflowY: 'auto', overflowX: 'none'}}> 
           <Typography color={(props.id === 0 ? "primary" : "secondary")}>{props.date}</Typography>
         </Grid>
-        <Grid item xs={2}>
+        <Grid item xs={2} style={{maxHeight: '10vh', overflow: 'auto'}}>
           <Typography color={(props.id === 0 ? "primary" : "secondary")}>{props.location}</Typography>
         </Grid>
-        <Grid item xs={2}>
+        <Grid item xs={2} style={{maxHeight: '10vh', overflow: 'auto'}}>
           <Typography color={(props.id === 0 ? "primary" : "secondary")}>{props.party}</Typography>
         </Grid>
-        <Grid item xs={2}>
+        <Grid item xs={2} style={{maxHeight: '10vh', overflow: 'auto'}}>
           <Typography color={(props.id === 0 ? "primary" : "secondary")}>{props.subject}</Typography>
         </Grid>
         <Grid item xs={4} style={{maxHeight: '10vh', overflow: 'auto'}}>
